9df7952b9ec3b6f8826283ec8da213d7a009577a
Julie_LECHARTIER <julie.lechartier@gmail.com>
2024-02-21 15:54:43 +0100

    Ajoutez des fichiers projet.

---
+++ Generateur_mot_passe/Outils.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace nombre_demander
+{
+    static class Outils
+    {
+        public static int DemandeNombrePositifNonNul(string question)
+        {
+
+            /*int nombre = DemanderNombre(question);
+            if (nombre > 0)
+            {
+                return nombre;
+            }
+            Console.WriteLine("ERREUR : le mot de passe ne peut pas être égale à 0;");
+            
+            
+
+            return DemandeNombrePositifNonNul(question);*/
+            return DemanderNombreEntre(question, 1, int.MaxValue);
+        }
+        public static int DemanderNombreEntre(string question, int min, int max)
+        {
+
+            int nombre = DemanderNombre(question);
+            if ((nombre >= min) && (nombre <= max))
+            {
+                return nombre;
+            }
+            else
+            {
+                Console.WriteLine("ERREUR : le nombre doit être compris entre " + min + " et " + max);
+            }
+
+
+            return DemanderNombreEntre(question, min, max);
+        }
+        static int DemanderNombre(string question)
+        {
+
+            while (true)
+            {
+                Console.Write(question);
+                string reponse = Console.ReadLine();
+                try
+                {
+                    int reponseInt = int.Parse(reponse);
+                    return reponseInt;
+
+                }
+                catch
+                {
+                    Console.WriteLine("ERREUR !");
+
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Generateur_mot_passe/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using nombre_demander;
+
+namespace generateur_mot_passe
+{
+    
+    class program
+    {
+        
+
+        static void Main(string[] args) 
+        {
+            const int NB_MOT_DE_PAPSSE = 10;
+            
+            // 1 Demander la longeur du mdp (demander Nombre)
+            int longueurMotDePasse = Outils.DemandeNombrePositifNonNul("Longueur du mot de passe:");
+            int choixAlpha = Outils.DemanderNombreEntre("Vous voulez un mot de passe avec :\n" +
+                "1- Uniquement des caractères en minuscules\n" +
+                "2- Des caractères minuscules et majuscules\n" +
+                "3- Des caractères et des chiffres\n" +
+                "4- Caractères, chiffres et caractères spéciaux \n" +
+                "Votre choix : ", 1, 4);
+
+            string minuscules = "abcdefghijklmnopqrstuvwxyz";
+
+            string majuscules = minuscules.ToUpper();
+            string chiffres = "0123456789";
+            string caractSpeciaux = "&#+-_!?./)({}|";
+            string alphabet;
+            string motDePasse = "";
+            
+            
+           
+            Random rand = new Random();
+
+            if (choixAlpha == 1)
+            {
+                alphabet = minuscules;
+            }
+            else if (choixAlpha == 2)
+            {
+                alphabet = minuscules + majuscules;
+            }
+            else if (choixAlpha == 3)
+            {
+                alphabet = minuscules + majuscules + chiffres;
+            }
+            else
+            {
+                alphabet = minuscules + majuscules + chiffres + caractSpeciaux;
+            }
+            
+            int longeurAlpha = alphabet.Length;
+
+            for (int x = 0; x < NB_MOT_DE_PAPSSE; x++)
+            {
+                motDePasse = "";
+                for (int i = 0; i < longueurMotDePasse; i++)
+                {
+                    int index = rand.Next(0, longeurAlpha);
+                    motDePasse += alphabet[index];
+                }
+
+
+                Console.WriteLine("Mot de passe : " + motDePasse);
+            }
+      
+        }
+
+    }
+}
